7f6be51b5c73480611d3689b00d009445b0e001e
Justin Thomas <justin.thomas1@gmail.com>
2016-08-29 15:13:59 -0700

    Demo source file as example

---
+++ Demo.t.java
@@ -0,0 +1,20 @@
+/***
+ * Excerpted from "The Definitive ANTLR 4 Reference",
+ * published by The Pragmatic Bookshelf.
+ * Copyrights apply to this code. It may not be used to create training material, 
+ * courses, books, articles, and the like. Contact us if you are in doubt.
+ * We make no guarantees that this code is fit for any purpose. 
+ * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
+***/
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+public class Demo {
+	void f(int x, String y) { }
+	int[ ] g(/*no args*/) { return null; }
+	List<Map<String, Integer>>[] h() { return null; }
+  int t() { List l = new ArrayList<>(); return 1; }
+  public static void main(String []args) {
+    System.out.println("hi");
+  }
+}
